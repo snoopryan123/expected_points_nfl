236439d2688a4afacb461622276c48cdfc6cfdcb
Ryan Brill <ryansbrill@gmail.com>
2025-03-05 21:44:57 -0500

    R_run_train_epochEP_models_for_testing_AJ

--- model_comparison/R_run_train_epochEP_models_for_testing_AJ.sh
+++ model_comparison/R_run_train_epochEP_models_for_testing_AJ.sh
@@ -4,9 +4,9 @@
 ####$ -m e -M ryguy123@sas.upenn.edu 
 
 ## ARRAY JOB
-#$ -t  33-101 #1-101
+#$ -t  1-101
 #$ -o job_output/$JOB_NAME-$JOB_ID-$TASK_ID.log
 ## MORE RAM
-#$ -l m_mem_free=20G
+#$ -l m_mem_free=15G
 
 Rscript --vanilla D_eval_EP_TrainModels.R TRUE FALSE FALSE ${SGE_TASK_ID} 101
